1f9ada61d292882922514c6ac9ae78694760de1a
Michael J. Thompson, CPACC <thompsonmichaelj2020@hotmail.com>
2023-01-24 11:51:59 -0500

    Fix: soda choices now display

--- README.md
+++
@@ -1,1 +0,0 @@
-# SodaMachineDebugging-main_lab
---
+++ sandbox.py
@@ -0,0 +1,9 @@
+
+# def forLoop():
+can_list = [2,3,344,44,5,565,6,65,34,4,544,45,55,5454,45,56,7]
+count = 0
+for can in can_list:
+    count += 1
+    if count  < 0:
+        print("zero")
+print (count)
